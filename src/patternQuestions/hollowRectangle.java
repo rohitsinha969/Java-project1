627191462b24f22185aec2ef292823781ebb6db9
rohitsinha969 <102020637+rohitsinha969@users.noreply.github.com>
2023-01-30 08:41:53 +0530

    package-cm3

--- src/patternQuestions/hollowRectangle.java
+++ src/patternQuestions/hollowRectangle.java
@@ -5,20 +5,25 @@ import java.util.Scanner;
 public class hollowRectangle {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("row value");
         int row = scan.nextInt();
+        System.out.println("col value");
         int col = scan.nextInt();
 
-        for(int i=0; i<row; i++){
-            if(row==0 || row==row-1){
-                for(int j=col; j<col; j++){
-                    System.out.print("*" +" ");
+        for (int i = 0; i < row; i++) {
+            if (i == 0 || i == row - 1) {
+                for (int j = 0; j < col; j++) {
+                    System.out.print("*" + " ");
                 }
-                System.out.println();
-            }else{
-                System.out.print("*" +" ");
+            } else {
+                System.out.print("*" + " ");
+                for (int j = 0; j < col - 2; j++) {
+                    System.out.print(" " + " ");
+                }
+                System.out.print("*" + " ");
             }
+            System.out.println();
         }
-
 
         scan.close();
     }
