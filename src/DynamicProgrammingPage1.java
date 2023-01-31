bf4607d933502e83017af84ba2cba6069466ed1c
rohitsinha969 <102020637+rohitsinha969@users.noreply.github.com>
2023-01-31 23:19:08 +0530

    DP-commit2

--- src/DynamicProgrammingPage1.java
+++ src/DynamicProgrammingPage1.java
@@ -1,5 +1,8 @@
 public class DynamicProgrammingPage1 {
+
+    // Dynamic Programming Methods to find Nth Fibonacci Number
     public static int fibonacci(int n, int[] dp) {
+        return n;
         /*
          * // Recursion + Memoization Approach
          * // Top-Down Approach in DP
@@ -11,26 +14,40 @@ public class DynamicProgrammingPage1 {
          * return dp[n];
          */
 
-        // Tabulation Approach
-        // Bottom-Up Approach in DP
-        if (n == 0 || n == 1) {
-            return n;
-        }
-        dp[0] = 0;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-        return dp[n];
+        /*
+         * // Tabulation Approach
+         * // Bottom-Up Approach in DP
+         * if (n == 0 || n == 1) {
+         * return n;
+         * }
+         * dp[0] = 0;
+         * dp[1] = 1;
+         * 
+         * for (int i = 2; i <= n; i++) {
+         * dp[i] = dp[i - 1] + dp[i - 2];
+         * }
+         * return dp[n];
+         */
     }
 
     public static void main(String[] args) {
-        int n = 5;
-        int dp[] = new int[n + 1];
-        for (int i = 0; i < dp.length; i++) {
-            dp[i] = -1;
+        int n = 7;
+        /*
+         * int dp[] = new int[n + 1];
+         * for (int i = 0; i < dp.length; i++) {
+         * dp[i] = -1;
+         * }
+         */
+
+        // Space Optimisation method
+        int prev1 = 1;
+        int prev2 = 0;
+        for (int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        System.out.println(fibonacci(n - 1, dp));
+        // System.out.println(fibonacci(n - 1, dp));
+        System.out.println(prev1);
     }
 }
