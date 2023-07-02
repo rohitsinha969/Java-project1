8622c4fee7eb1330a8915f9bc4742c1da333c5cd
rohitsinha969 <102020637+rohitsinha969@users.noreply.github.com>
2023-07-02 20:02:36 +0530

    package-commit14

--- src/LinkedLists_in_Java.java
+++ src/LinkedLists_in_Java.java
@@ -16,30 +16,66 @@ public class LinkedLists_in_Java {
         }
     }
 
-    public static Node insertAtHead(Node head, int data) {
-        // steps involved :
-        // 1 - create a NewNode whenever needed
-        // 2 - point the next of NewNode to Head
-        // 3 - update the Head to NewNode so next time more Nodes gets attached to Head
+    public static class LinkedList {
+        Node head;
+        Node tail;
 
-        // step 1
-        Node newNode = new Node(data);
-        // step 2
-        newNode.next = head;
-        // step 3
-        // head = newNode;
-        return newNode;
-    }
-
-    // function to print Linked List
-    public static void printLinkedList(Node head) {
-        Node curr = head;
-        while (curr != null) {
-            System.out.print(curr.data + " --> ");
-            curr = curr.next;
+        LinkedList() {
+            this.head = null;
+            this.tail = null;
         }
-        System.out.println("Null");
+
+        public void insertAtHead(int data) {
+            // steps involved :
+            // 1 - create a NewNode whenever needed
+            // 2 - point the next of NewNode to Head
+            // 3 - update the Head to NewNode so next time more Nodes gets attached to Head
+
+            // step 1
+            Node newNode = new Node(data);
+            // step 2
+            newNode.next = head;
+            // step 3
+            head = newNode;
+            if (tail == null) {
+                tail = newNode;
+            }
+        }
+
+        public void insertAtTail(int data) {
+            // steps involved :
+            // 1 - create a NewNode whenever needed
+            // 2 - point the next of NewNode to Head
+            // 3 - update the Head to NewNode so next time more Nodes gets attached to Head
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        public void printLinkedList() {
+            Node curr = head;
+            while (curr != null) {
+                System.out.print(curr.data + " --> ");
+                curr = curr.next;
+            }
+            System.out.println("Null");
+        }
     }
+
+    // // function to print Linked List
+    // public static void printLinkedList(Node head) {
+    // Node curr = head;
+    // while (curr != null) {
+    // System.out.print(curr.data + " --> ");
+    // curr = curr.next;
+    // }
+    // System.out.println("Null");
+    // }
 
     public static void main(String[] args) {
 
@@ -61,13 +97,25 @@ public class LinkedLists_in_Java {
          * 
          */
 
-        Node head = new Node(10);
-        head = insertAtHead(head, 20);
-        head = insertAtHead(head, 30);
-        head = insertAtHead(head, 40);
-        head = insertAtHead(head, 50);
+        // Node head = new Node(10);
+        // Node tail = new Node();
+        // head = insertAtHead(head, 20);
+        // head = insertAtHead(head, 30);
+        // head = insertAtHead(head, 40);
+        // head = insertAtHead(head, 50);
+
+        LinkedList ll = new LinkedList();
+        ll.insertAtHead(3);
+        ll.insertAtHead(2);
+
+        ll.insertAtTail(1);
+        ll.insertAtTail(2);
+        ll.insertAtTail(3);
+        ll.insertAtTail(4);
+        ll.insertAtTail(5);
+
         // printing the Linked List by function call
         System.out.println("Linked List is : ");
-        printLinkedList(head);
+        ll.printLinkedList();
     }
 }
